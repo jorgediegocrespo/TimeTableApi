49d769984463a49ed699dc8d81df62aaeb58986c
Jorge Diego <jorge@devsdna.com>
2021-12-09 14:40:31 +0100

    Seed refactor

---
+++ TimeTable.DataAccess/Seed/PeopleSeed.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTable.Business.ConstantValues;
+using TimeTable.DataAccess.Contracts.Entities;
+
+namespace TimeTable.DataAccess.Seed
+{
+    public class PeopleSeed
+	{
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public PeopleSeed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task SeedAsync(TimeTableDbContext context, IConfiguration configuration)
+		{
+			await AddDefaultCompany(context);
+		}
+
+		private async Task AddDefaultCompany(TimeTableDbContext context)
+		{
+			if (context.People.Any())
+				return;
+
+			using (var transaction = await context.Database.BeginTransactionAsync())
+			{
+				try
+				{
+					IdentityUser user = new IdentityUser
+					{
+						UserName = "Admin",
+						Email = "admin@email.com",
+					};
+
+					await userManager.CreateAsync(user, "Admin_1234");
+					await userManager.AddToRoleAsync(user, RolesConsts.ADMIN);
+
+					string userId = await userManager.GetUserIdAsync(user);
+					await context.People.AddAsync(new PersonEntity() { Name = "Admin", UserId = userId });
+
+					await context.SaveChangesAsync();
+
+					await transaction.CommitAsync();
+				}
+				catch
+				{
+					await transaction.RollbackAsync();
+					throw;
+				}
+			}
+		}
+	}
+}
--- TimeTable.DataAccess/Seed/RolesSeed.cs
+++ TimeTable.DataAccess/Seed/RolesSeed.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using TimeTable.Business.ConstantValues;
-using TimeTable.DataAccess.Contracts.Entities;
 
 namespace TimeTable.DataAccess.Seed
 {
@@ -29,56 +27,6 @@ namespace TimeTable.DataAccess.Seed
 
 			await roleManager.CreateAsync(new IdentityRole(RolesConsts.ADMIN));
 			await roleManager.CreateAsync(new IdentityRole(RolesConsts.EMPLOYEE));
-		}
-	}
-
-	public class PeopleSeed
-	{
-        private readonly UserManager<IdentityUser> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
-
-        public PeopleSeed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
-        {
-            this.userManager = userManager;
-            this.roleManager = roleManager;
-        }
-
-        public async Task SeedAsync(TimeTableDbContext context, IConfiguration configuration)
-		{
-			await AddDefaultCompany(context);
-		}
-
-		private async Task AddDefaultCompany(TimeTableDbContext context)
-		{
-			if (context.People.Any())
-				return;
-
-			using (var transaction = await context.Database.BeginTransactionAsync())
-			{
-				try
-				{
-					IdentityUser user = new IdentityUser
-					{
-						UserName = "Admin",
-						Email = "admin@email.com",
-					};
-
-					await userManager.CreateAsync(user, "Admin_1234");
-					await userManager.AddToRoleAsync(user, RolesConsts.ADMIN);
-
-					string userId = await userManager.GetUserIdAsync(user);
-					await context.People.AddAsync(new PersonEntity() { Name = "Admin", UserId = userId });
-
-					await context.SaveChangesAsync();
-
-					await transaction.CommitAsync();
-				}
-				catch
-				{
-					await transaction.RollbackAsync();
-					throw;
-				}
-			}
 		}
 	}
 }
